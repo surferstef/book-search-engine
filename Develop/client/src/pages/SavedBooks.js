dfffc5e721fcd3dc265ad384ea733b2b56442455
stefan hall <stefhall@comcast.net>
2021-08-22 17:15:45 -0700

    Update to SavedBooks.js

--- Develop/client/src/pages/SavedBooks.js
+++ Develop/client/src/pages/SavedBooks.js
@@ -1,12 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-
+import { useQuery, useMutation } from "@apollo/client";
+import { GET_ME } from "../utils/queries";
+import { REMOVE_BOOK } from "../utils/mutations";
 import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
+// const SavedBooks = () => {
+//   const [userData, setUserData] = useState({});
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+  const { loading, data } = useQuery(GET_ME);
+  const [deleteBook] = useMutation(REMOVE_BOOK);
+  const userData = data?.me || {};
+  if (!userData?.username) {
+    return (
+      <h4>
+        You need to be logged in to see this page. Use the navigation links
+        above to sign up or log in!
+      </h4>
+    );
+  }
 
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
@@ -37,33 +51,43 @@ const SavedBooks = () => {
   }, [userDataLength]);
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
+
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
     if (!token) {
       return false;
     }
-
-    try {
-      const response = await deleteBook(bookId, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
-      // upon success, remove book's id from localStorage
-      removeBookId(bookId);
-    } catch (err) {
-      console.error(err);
+  try {
+    await deleteBook({
+       variables: { bookId: bookId },
+       update: (cache) => {
+        const data = cache.readQuery({ query: GET_ME });
+        const userDataCache = data.me;
+        const savedBooksCache = userDataCache.savedBooks;
+        const updatedBookCache = savedBooksCache.filter(
+          (book) => book.bookId !== bookId
+       );
+         data.me.savedBooks = updatedBookCache;
+        cache.writeQuery({
+           query: GET_ME,
+           data: { data: { ...data.me.savedBooks } },
+        });
+       },
+   });
+  // upon success, remove book's id from localStorage
+         removeBookId(bookId);
+    }     catch (err) {
+          console.error(err);
     }
   };
 
-  // if data isn't here yet, say so
-  if (!userDataLength) {
-    return <h2>LOADING...</h2>;
+  if (loading) {
+   return <h2>LOADING...</h2>;
   }
+
+  // if data isn't here yet, say so
+
 
   return (
     <>
