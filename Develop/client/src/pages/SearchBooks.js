d8d497bd13845db539f1a0c7721dfdc8040d1da6
stefan hall <stefhall@comcast.net>
2021-08-22 17:03:54 -0700

    Updates to SearchBooks.js

--- Develop/client/src/pages/SearchBooks.js
+++ Develop/client/src/pages/SearchBooks.js
@@ -3,12 +3,16 @@ import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'reac
 
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
-import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
+import { GET_ME } from '../utils/queries';
+import { saveBookIds, getSavedBookIds, removeBookId } from '../utils/localStorage';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
+
   const [searchInput, setSearchInput] = useState('');
 
   // create state to hold saved bookId values
@@ -20,6 +24,7 @@ const SearchBooks = () => {
     return () => saveBookIds(savedBookIds);
   });
 
+  const [saveBook] = useMutation(SAVE_BOOK);
   // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
     event.preventDefault();
@@ -65,14 +70,19 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const { data } = await savedBookId({
+        variables: { bookData: { ...bookToSave } },
+      });
+      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+     //  const response = await saveBook(bookToSave, token);
+
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
 
       // if book successfully saves to user's account, save book id to state
-      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+    
     } catch (err) {
       console.error(err);
     }
